59e4366ee4eefc184e74db81abe25cceed633f66
yocallmed <histind@gmail.com>
2014-03-24 00:34:00 -0500

    added rpi_task_gpio
    rpi_task_gpio will be the base file for security tasks.

---
+++ src/rpi_task_gpio.c
@@ -0,0 +1,43 @@
+#include <sys/mman.h>
+#include <native/task.h>
+#include <wiringPi.h>
+// task parameters
+#define TASK_PRIO 99 /* Highest RT priority */
+#define TASK_MODE 0 /* No flags */
+#define TASK_STKSZ 0 /* Stack size (use default one) */
+// input and output pins
+#define INPUT_PIN 0
+#define OUTPUT_PIN 1
+RT_TASK task_desc;
+void task_body (void *cookie)
+{
+for (;;) {
+// poll input pin for trigger
+if(digitalRead(INPUT_PIN
+// when triggered, do security task
+// ...else continue
+
+// send response on output pin
+
+continue;
+}
+}
+int main (int argc, char *argv[])
+{
+int err;
+// turn off paging
+mlockall(MCL_CURRENT|MCL_FUTURE);
+/* ... */
+err = rt_task_create(&task_desc,
+"SRT_task",
+TASK_STKSZ,
+TASK_PRIO,
+TASK_MODE);
+if (!err)
+rt_task_start(&task_desc,&task_body,NULL);
+/* ... */
+}
+void cleanup (void)
+{
+rt_task_delete(&task_desc);
+}
