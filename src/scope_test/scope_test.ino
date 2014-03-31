9ad854f21fe62e9d7acb7f99494728a77c66439f
yocallmed <histind@gmail.com>
2014-03-30 22:22:48 -0500

    added setups and edited rpi_task_gpio
    setup files and rpi_task_gpio, csv_reader

--- src/scope_test/scope_test.ino
+++ src/scope_test/scope_test.ino
@@ -3,18 +3,31 @@ void setup() {
   Serial.begin(9600);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
+  //DDRD = B00011000;
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(3, HIGH);
-  delay(250);
-  digitalWrite(3, LOW);
-  delay(125);
-  if((Serial.available() > 0) && (Serial.read() == 's')){
+  //digitalWrite(3, HIGH);
+  //delay(250);
+  //digitalWrite(3, LOW);
+  //delay(125);
+  if((Serial.available() > 0) && (Serial.read() == 't')){
   Serial.println("trigger");
-  digitalWrite(4, HIGH);
-  //delay(5);
-  digitalWrite(4, LOW);
+  while(!Serial.available() > 0){
+    // 20 uS to toggle pins
+    digitalWrite(4, HIGH);
+    digitalWrite(4, LOW);
+    //PORTD = B00010000;
+    //PORTD = B00000000;
+    // similate rpi delay
+    // digitalRead(3);
+    // resolution of 4 uS
+    delay(20);
+    digitalWrite(3, HIGH);
+    digitalWrite(3, LOW);
+    //PORTD = B00001000;
+    //PORTD = B00000000;
+    }
   }
-  delay(125);
+  //delay(125);
 }
