3618fded5d109de1be5fb48deef58037f8cb0c77
yocallmed <histind@gmail.com>
2014-04-01 22:30:27 -0500

    created scope_test_due

--- src/scope_test_due/scope_test_due.ino
+++ src/scope_test_due/scope_test_due.ino
@@ -7,29 +7,22 @@ void setup() {
   //DDRD = B00011000;
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-  //digitalWrite(3, HIGH);
-  //delay(250);
-  //digitalWrite(3, LOW);
-  //delay(125);
   if((Serial.available() > 0) && (Serial.read() == 't')){
   Serial.println("trigger");
   while(!Serial.available() > 0){
     // 20 uS to toggle pins
     digitalWrite(4, HIGH);
-    //digitalWrite(4, LOW);
-    //PORTD = B00010000;
-    //PORTD = B00000000;
+    digitalWrite(4, LOW);
     // due pin on/off:
     //g_APinDescription[4].pPort -> PIO_SODR = g_APinDescription[4].ulPin;
     //g_APinDescription[4].pPort -> PIO_CODR = g_APinDescription[4].ulPin;    // similate rpi delay
     // digitalRead(3);
     // resolution of 4 uS
     delay(20);
+    
     digitalWrite(3, HIGH);
-    //digitalWrite(3, LOW);
-    //PORTD = B00001000;
-    //PORTD = B00000000;
+    digitalWrite(3, LOW);
+    delay(20);
     // due pin on/off:
     //g_APinDescription[3].pPort -> PIO_SODR = g_APinDescription[3].ulPin;
     //g_APinDescription[3].pPort -> PIO_CODR = g_APinDescription[3].ulPin;
