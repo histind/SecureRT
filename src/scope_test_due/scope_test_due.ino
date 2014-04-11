7beb84dcebb2ee308586c3cc85ec477634b81757
yocallmed <histind@gmail.com>
2014-04-10 19:38:55 -0500

    change write method in scope_test_due

--- src/scope_test_due/scope_test_due.ino
+++ src/scope_test_due/scope_test_due.ino
@@ -16,6 +16,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(3, INPUT);
   pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
   restart = 1;
   data = 0;
 }
@@ -42,15 +43,17 @@ void debug_mode(){
 void test_mode(){
    while(1){
      digitalWrite(4, HIGH);
-     digitalWrite(4, LOW);
+     //digitalWrite(4, LOW);
      while(digitalRead(3) != HIGH){
        Serial.println("waiting for response on pin 3...");
        if(Serial.available() > 0){
+         digitalWrite(4, LOW);
          restart = 1;
          return;
        }
      }
      // digitalRead is HIGH
+     digitalWrite(4, LOW);
      data++;
      print_result();
      // wait for pin 3 to release signal
@@ -64,12 +67,8 @@ void help(){
   Serial.println("press 'd' for debug mode");
   Serial.println("      't' for test mode");
   Serial.println("      'r' for results");
-  while(1){
-     if(Serial.available() > 0){
-        restart = 0;
-        return;
-     } 
-  }
+  restart = 0;
+  return;
   
 }
 
@@ -77,6 +76,7 @@ void print_result(){
    sprintf (result, "number of samples is %d", data);
    Serial.println(result);
    restart = 0;
+   return;
 }
 void loop() {
   // put your main code here, to run repeatedly
